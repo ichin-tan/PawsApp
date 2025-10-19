7423e8322d57db56b0f0c2cd2340b3d9f52d5679
ichin-tan <chintanpanchal396@gmail.com>
2025-10-19 16:58:23 -0400

    initial commit

---
+++ PawsInfo/PawsInfo/PawsInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsInfoApp.swift
+//  PawsInfo
+//
+//  Created by CP on 19/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PawsInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
